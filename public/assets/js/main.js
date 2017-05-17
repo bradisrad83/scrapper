b94dc7807f88bb8df53c1e15e5c4aee5f0543c01
Brad Goldsmith <b-rad@Brads-MacBook-Pro.local>
2017-05-17 16:33:40 -0400

    Starting work on note modals but am confused

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
   //API AJAX call when save button clicked
-  $(".save").on("click", function(event){
+  $(".save").on("click", function(event) {
     var btn = $(this);
     var id = btn.attr("data-id");
     $.ajax({
-      url: "/"+ id,
+      url: "/" + id,
       method: "PUT"
-    }).done(function(data){
-      if(data.success===true){
+    }).done(function(data) {
+      if (data.success === true) {
         console.log("Saved Successfully");
         btn.addClass("hidden");
       }
-    })
+    });
   });
-
+  $(".addNote").on("click", function(event){
+    console.log("hitting note");
+    $("#myModal").modal("show");
+  });
 });
